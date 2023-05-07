397a819e4b8f93c0437daffcb5a0b874a8497f69
rmpasta <rmpasta.code@gmail.com>
2023-05-06 21:32:14 -0400

    get first thunk and new reducer working

--- react-app/src/components/Users/Users.js
+++ react-app/src/components/Users/Users.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllUsers } from '../../store/users';
-// import './Users.css'
+import './Users.css'
 
 function Users(){
     const dispatch = useDispatch();
