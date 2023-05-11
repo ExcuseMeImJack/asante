4ea6b9e669bc138e79370d177b0230b613c833db
David Kim <dhskim22@gmail.com>
2023-05-11 10:19:39 -0400

    set up restart

--- react-app/src/components/Sections/Sections.js
+++ react-app/src/components/Sections/Sections.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getSectionsByBoardId } from '../../store/sections';
+import { getSectionsByBoardId, orderSections } from '../../store/sections';
 import { useHistory, useParams } from 'react-router-dom';
 import AllTasksBySection from '../Tasks/AllTasksBySection';
 import './Sections.css'
 import { deleteSectionById } from '../../store/sections';
+import { Droppable, Draggable, DragDropContext } from 'react-beautiful-dnd';
+
 
 function Sections() {
     const { boardId } = useParams();
@@ -17,6 +19,30 @@ function Sections() {
         dispatch(getSectionsByBoardId(boardId))
     }, [dispatch, boardId, storeSections.sections.length])
 
+    const onDragEnd = (result) => {
+        const {destination, source, draggableId} = result;
+        console.log('Source ~~~~~~~~~>', source)
+        console.log('Destination ~~~~~~~~~>', destination)
+        console.log('DraggableId ~~~~~~~~~~~~~~~>', draggableId)
+        if (!destination) {
+            return;
+        }
+
+        if (
+            destination.dropableId === source.droppableId &&
+            destination.index === source.index
+        ) {
+            return;
+        }
+
+
+        const sections = [ ...storeSections.sections ]
+        const section = sections[source.index]
+        sections.splice(source.index, 1)
+        sections.splice(destination.index, 0, section)
+
+        dispatch(orderSections(sections, boardId))
+    }
 
     // grab sections array from the storeSections object
     const sections = storeSections.sections;
@@ -38,6 +64,36 @@ function Sections() {
                     </div>
                 })}
             </div>
+        </div>
+    );
+}
+
+export default Sections;
+
+function Sections() {
+    return (
+        <div>
+            <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="ROOT" direction='horizontal'>
+                {(provided) => (
+                    <div className='section-gallery' {...provided.droppableProps} ref={provided.innerRef}>
+                    {sections.map((section, index) => (
+                        <Draggable draggableId={"section-" + section.id} key={section.id} index={index}>
+                        {(provided) => (
+                            <div className='single-section-border' ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                <div>
+                                    <div>{section.name}</div>
+                                    <AllTasksBySection sectionId={section.id}/>
+                                </div>
+                            </div>
+                        )}
+                        </Draggable>
+                    ))}
+                    {provided.placeholder}
+                </div>
+                )}
+            </Droppable>
+            </DragDropContext>
         </div>
     );
 }
