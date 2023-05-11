f8136bf14d9b86c0fe204a44447dab439f8ff7f8
kariyona <princesswyona@gmail.com>
2023-05-11 12:57:23 -0700

    reverted singletask to its original state

--- react-app/src/components/Tasks/SingleTask.js
+++ react-app/src/components/Tasks/SingleTask.js
@@ -1,30 +1,19 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getTaskById } from "../../store/tasks";
-import { useParams } from "react-router-dom";
-import "./SingleTask.css";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getTaskById } from '../../store/tasks';
+import { useParams } from 'react-router-dom';
+import './SingleTask.css'
 
-function SingleTask() {
-  const dispatch = useDispatch();
-  const { taskId } = useParams();
-  const storeTasks = useSelector((state) => state.tasks);
+function SingleTask({task}){
 
-  useEffect(() => {
-    dispatch(getTaskById(taskId));
-  }, [dispatch]);
+    if (!task) return <h1>...Loading</h1>
 
-  const task = storeTasks.task;
-
-  if (!task) return <h1>...Loading</h1>;
-
-  return (
-    <div className="single-task-container">
-      <div className="single-task-card">
-        <h3>{task.name}</h3>
-        <div>{task.description || "No description"}</div>
-      </div>
-    </div>
-  );
+	return (
+        <div className='single-task-card'>
+            <h3>{task.name}</h3>
+            <div>{task.description || "No description"}</div>
+        </div>
+	);
 }
 
 export default SingleTask;
