401cf81e7d1b378e2d56388abb49c8332ba1d18a
kariyona <princesswyona@gmail.com>
2023-05-11 16:01:38 -0700

    homepage edited

--- react-app/src/components/Users/UserProfile.js
+++ react-app/src/components/Users/UserProfile.js
@@ -1,139 +1,161 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getUserProfile } from '../../store/users';
-import EditProfileModal from '../EditProfileModal';
-import OpenModalButton from '../OpenModalButton';
-import './UserProfile.css'
-import { getTasksByUserId } from '../../store/tasks';
-import { getBoardsByUserId } from '../../store/boards';
-import DeleteUserModal from '../DeleteUserModal';
-import quoteCensor from './quoteCensor';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserProfile } from "../../store/users";
+import EditProfileModal from "../EditProfileModal";
+import OpenModalButton from "../OpenModalButton";
+import "./UserProfile.css";
+import { getTasksByUserId } from "../../store/tasks";
+import { getBoardsByUserId } from "../../store/boards";
+import DeleteUserModal from "../DeleteUserModal";
+import quoteCensor from "./quoteCensor";
+import { Link } from "react-router-dom";
 
-function UserProfile(){
-    const dispatch = useDispatch();
-    const profile = useSelector((state) => state.users.profile);
-    const tasks = useSelector((state) => state.tasks.tasks);
-    const boards = useSelector((state) => state.boards.boards);
-    const [quoteInfo, setQuoteInfo] = useState("");
+function UserProfile() {
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state.users.profile);
+  const tasks = useSelector((state) => state.tasks.tasks);
+  const boards = useSelector((state) => state.boards.boards);
+  const [quoteInfo, setQuoteInfo] = useState("");
 
-    //dispatch thunk to populate storeUsers variable
-    useEffect(() => {
-        dispatch(getUserProfile())
-        dispatch(getTasksByUserId())
-        dispatch(getBoardsByUserId())
-    }, [dispatch])
+  //dispatch thunk to populate storeUsers variable
+  useEffect(() => {
+    dispatch(getUserProfile());
+    dispatch(getTasksByUserId());
+    dispatch(getBoardsByUserId());
+  }, [dispatch]);
 
-    useEffect(() => {
-        const fetchQuote = async () => {
-            const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=success',{
-                headers: {'X-Api-Key': 'WrkXdZlorsWnOkvJdFoc9Q==oNhDvwTXKFJguwGD'},
-                contentType: 'application/json'
-            })
-            // console.log('QUOTE RES', res)
-            if(res.ok){
-                const data = await res.json()
-                setQuoteInfo(data)
-
-            } else {
-                return('error', res.error)
-            }
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const res = await fetch(
+        "https://api.api-ninjas.com/v1/quotes?category=success",
+        {
+          headers: { "X-Api-Key": "WrkXdZlorsWnOkvJdFoc9Q==oNhDvwTXKFJguwGD" },
+          contentType: "application/json",
         }
-        fetchQuote()
-    }, [dispatch])
+      );
+      // console.log('QUOTE RES', res)
+      if (res.ok) {
+        const data = await res.json();
+        setQuoteInfo(data);
+      } else {
+        return "error", res.error;
+      }
+    };
+    fetchQuote();
+  }, [dispatch]);
 
-    if (!profile) return <h1>...Loading</h1>
+  if (!profile) return <h1>...Loading</h1>;
 
-    if (quoteInfo[0]){
-        const quote = quoteInfo[0].quote;
-        const author = quoteCensor(quoteInfo[0].author);
+  if (quoteInfo[0]) {
+    const quote = quoteInfo[0].quote;
+    const author = quoteCensor(quoteInfo[0].author);
 
-
-        return (
-            <div className='profile-page'>
-                <div className='profile-info-container'>
-                    <div className='profile-pic-container'>
-                        <img id='profile-pic-image' src={profile.profile_pic_url}/>
-                    </div>
-                    <div className='profile-info'>
-                        <h1>{profile.name}</h1>
-                        <div className='username-email-container'>
-                            <div className='username-container'>
-                                <p>@</p>
-                                <p>{profile.username} |</p>
-                            </div>
-                            <div className='email-container'>
-                                <i className="fa-regular fa-envelope"></i>
-                                <p>{profile.email}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='edit-delete-profile-button-div'>
-                        <OpenModalButton
-                            buttonStyleClass={"edit-profile-button change-cursor"}
-                            buttonText={"Edit profile"}
-                            modalComponent={<EditProfileModal/>}
-                            modalStyleClass={'edit-profile-modal-content'}
-                        />
-                        <OpenModalButton
-                            buttonStyleClass={"delete-user-button change-cursor"}
-                            buttonText={"Delete user"}
-                            modalComponent={<DeleteUserModal/>}
-                            modalStyleClass={'delete-profile-modal-content'}
-                        />
-                    </div>
-                </div>
-                <div className='profile-tiles-container'>
-                    <div className='profile-page-div-1'>
-                        <div id='profile-boards-container'>
-                            <h2>My Boards</h2>
-                            <div className='profile-user-boards-container'>
-                                {boards ?
-                                    boards.map(board => (
-                                        <div className='profile-user-board-tile' key={board.id}>
-                                            <div className='profile-divider'></div>
-                                            <Link to={`/boards/${board.id}`} id='profile-board-link' className='change-cursor'>{board.name}</Link>
-                                        </div>
-                                    ))
-                                :
-                                <div><p>You have no boards</p></div>}
-                            </div>
-                            <div className='profile-divider'></div>
-                        </div>
-                        <div id='profile-tasks-container'>
-                            <h2>My Tasks</h2>
-                            <div className='profile-user-tasks'>
-                                {tasks ?
-                                        tasks.map(task => (
-                                            <div className='profile-user-task-tile' key={task.id}>
-                                                <div className='profile-divider'></div>
-                                                <Link to={`/tasks/${task.id}`} id='profile-task-link' className='change-cursor'>{task.name}</Link>
-                                            </div>
-                                        ))
-                                    :
-                                    <div><p>You have no tasks</p></div>}
-                            </div>
-                            <div className='profile-divider'></div>
-                        </div>
-
-                    </div>
-                    <div className='profile-page-div-2'>
-                            <div id='about-me-container'>
-                                <h2>About me</h2>
-                                <p>{profile.about_me ? profile.about_me : "Use this space to tell people about yourself."}</p>
-                            </div>
-                            <div id='quotes-container'>
-                                <h2>Need some motivation?</h2>
-                                <p>"{quote}"</p>
-                                <p id='author'>- {author}</p>
-                            </div>
-                        </div>
-                </div>
+    return (
+      <div className="profile-page">
+        <div className="profile-info-container">
+          <div className="profile-pic-container">
+            <img id="profile-pic-image" src={profile.profile_pic_url} />
+          </div>
+          <div className="profile-info">
+            <h1>{profile.name}</h1>
+            <div className="username-email-container">
+              <div className="username-container">
+                <p>@</p>
+                <p>{profile.username} |</p>
+              </div>
+              <div className="email-container">
+                <i className="fa-regular fa-envelope"></i>
+                <p>{profile.email}</p>
+              </div>
             </div>
-        );
-    } else {
-        return <h1>...Loading</h1>
-    }
+          </div>
+          <div className="edit-delete-profile-button-div">
+            <OpenModalButton
+              buttonStyleClass={"edit-profile-button change-cursor"}
+              buttonText={"Edit profile"}
+              modalComponent={<EditProfileModal />}
+              modalStyleClass={"edit-profile-modal-content"}
+            />
+            <OpenModalButton
+              buttonStyleClass={"delete-user-button change-cursor"}
+              buttonText={"Delete user"}
+              modalComponent={<DeleteUserModal />}
+              modalStyleClass={"delete-profile-modal-content"}
+            />
+          </div>
+        </div>
+        <div className="profile-tiles-container">
+          <div className="profile-page-div-1">
+            <div id="profile-boards-container">
+              <h2>My Boards</h2>
+              <div className="profile-user-boards-container">
+                {boards ? (
+                  boards.map((board) => (
+                    <div className="profile-user-board-tile" key={board.id}>
+                      <div className="profile-divider"></div>
+                      <Link
+                        to={`/boards/${board.id}`}
+                        id="profile-board-link"
+                        className="change-cursor"
+                      >
+                        {board.name}
+                      </Link>
+                    </div>
+                  ))
+                ) : (
+                  <div>
+                    <p>You have no boards</p>
+                  </div>
+                )}
+              </div>
+              <div className="profile-divider"></div>
+            </div>
+            <div id="profile-tasks-container">
+              <h2>My Tasks</h2>
+              <div className="profile-user-tasks">
+                {tasks ? (
+                  tasks.map((task) => (
+                    <div className="profile-user-task-tile" key={task.id}>
+                      <div className="profile-divider"></div>
+                      <Link
+                        to={`/tasks/${task.id}`}
+                        id="profile-task-link"
+                        className="change-cursor"
+                      >
+                        {task.name}
+                      </Link>
+                    </div>
+                  ))
+                ) : (
+                  <div>
+                    <p>You have no tasks</p>
+                  </div>
+                )}
+              </div>
+              <div className="profile-divider"></div>
+            </div>
+          </div>
+          <div className="profile-page-div-2">
+            <div id="about-me-container">
+              <h2>About me</h2>
+              <p>
+                {profile.about_me
+                  ? profile.about_me
+                  : "Use this space to tell people about yourself."}
+              </p>
+            </div>
+            <div id="quotes-container">
+              <h2>Need some motivation?</h2>
+              <p>"{quote}"</p>
+              <p id="author">- {author}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <h1>...Loading</h1>;
+  }
 }
 
 export default UserProfile;
