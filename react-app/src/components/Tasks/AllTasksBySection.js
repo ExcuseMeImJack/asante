2a36948d4a694e840dd7cc5fd592235784c87d45
David Kim <dhskim22@gmail.com>
2023-05-13 13:44:28 -0400

    finished merge dnd to team-b

--- react-app/src/components/Tasks/AllTasksBySection.js
+++ react-app/src/components/Tasks/AllTasksBySection.js
@@ -28,59 +28,59 @@ function AllTasksBySection({ section, boardId }) {
 
     const tasks = storeTasks.tasks.filter(task => task.section_id === sectionId).sort((a, b) => a.order - b.order)
 
-                return (
-                    <div>
-                        <div className='delete-warning-section'>
-                        <i className='fa-solid fa-trash section-trash' id="section-trash" onClick={async (e) => {
-                            e.preventDefault()
-                            if (!deleteClicked) {
-                                return setDeleteClicked(true)
-                            }
-                            // await dispatch(deleteSectionById(section))
-                            // return history.push(`/boards/${boardId}`)
-                        }}></i>
-                            {deleteClicked && <p className='delete-text-section'>Are you sure?</p>}
-                            <div>
+    return (
+        <div>
+            <div className='delete-warning-section'>
+                <i className='fa-solid fa-trash section-trash' id="section-trash" onClick={async (e) => {
+                    e.preventDefault()
+                    if (!deleteClicked) {
+                        return setDeleteClicked(true)
+                    }
+                    // await dispatch(deleteSectionById(section))
+                    // return history.push(`/boards/${boardId}`)
+                }}></i>
+                {deleteClicked && <p className='delete-text-section'>Are you sure?</p>}
+                <div>
                                 {deleteClicked && <i className='fa-solid fa-xmark' id="section-xmark" onClick={() => { setDeleteClicked(false)}}></i>}
-                                {deleteClicked && <i className='fa-solid fa-check' id="section-check" onClick={async () => {
-                                    await dispatch(deleteSectionById(section))
-                                    return history.push(`/boards/${boardId}`)
-                                }}></i>}
-                            </div>
-                        </div>
-                        {!editButton
-                            ? <i className="fa-solid fa-pen-to-square edit-section-button" id="pen" onClick={() => { setEditButton(true) }}></i>
-                            : <i className="fa-solid fa-xmark edit-section-button" id="xmark" onClick={() => { setEditButton(false) }}></i>}
-                        {!editButton
-                            ? <></>
-                            : <EditSectionForm sectionId={section.id} boardId={boardId} setEditButton={setEditButton} />}
-                    <div className='tasks-container'>
+                    {deleteClicked && <i className='fa-solid fa-check' id="section-check" onClick={async () => {
+                        await dispatch(deleteSectionById(section))
+                        return history.push(`/boards/${boardId}`)
+                    }}></i>}
+                </div>
+            </div>
+            {!editButton
+                ? <i className="fa-solid fa-pen-to-square edit-section-button" id="pen" onClick={() => { setEditButton(true) }}></i>
+                : <i className="fa-solid fa-xmark edit-section-button" id="xmark" onClick={() => { setEditButton(false) }}></i>}
+            {!editButton
+                ? <></>
+                : <EditSectionForm sectionId={section.id} boardId={boardId} setEditButton={setEditButton} />}
+            <div className='tasks-container'>
 
-                        {createButton
+                {createButton
                         ?  <i className="fa-solid fa-plus create-task-button" id="plus" onClick={() => { setCreateButton(false) }}></i>
-                        : <i className="fa-solid fa-minus create-task-button" id="minus" onClick={() => { setCreateButton(true) }}></i>}
-                        {createButton
+                    : <i className="fa-solid fa-minus create-task-button" id="minus" onClick={() => { setCreateButton(true) }}></i>}
+                {createButton
                         ?  <></>
                         :  <CreateTaskBySectionForm sectionId={section.id} setCreateButton={setCreateButton} />}
-                        {console.log('SECTION TASKS~~~~~~~', tasks.map(t => t.order))}
-                                    <Droppable droppableId={'section-' + sectionId} type='task'>
-                            {(provided) => (
-                                <div className='task-gallery' {...provided.droppableProps} ref={provided.innerRef}>
-                                    {tasks.map((task, index) => (
-                                        <Draggable draggableId={"task-" + task.id} key={task.id} index={index}>
-                                            {(provided) => (
-                                                < div className='single-task-border' ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
-                                                    <SingleTask task={task} />
-                                                </div>
-                                            )}
-                                        </Draggable>
-                                    ))}
-                                    {provided.placeholder}
-                                </div>
-                            )}
-                        </Droppable>
-                                </div >
-                </div>
+                {console.log('SECTION TASKS~~~~~~~', tasks.map(t => t.order))}
+                <Droppable droppableId={'section-' + sectionId} type='task'>
+                    {(provided) => (
+                        <div className='task-gallery' {...provided.droppableProps} ref={provided.innerRef}>
+                            {tasks.map((task, index) => (
+                                <Draggable draggableId={"task-" + task.id} key={task.id} index={index}>
+                                    {(provided) => (
+                                        < div className='single-task-border' ref={provided.innerRef} {...provided.dragHandleProps} {...provided.draggableProps}>
+                                            <SingleTask task={task} />
+                                        </div>
+                                    )}
+                                </Draggable>
+                            ))}
+                            {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </div >
+        </div>
     );
 }
 
