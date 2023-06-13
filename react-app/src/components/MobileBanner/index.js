33082d2c3f0378e8aad277e7370640a259d4cac1
rmpasta <rmpasta.code@gmail.com>
2023-06-12 23:42:49 -0400

    style mobile banner more

--- react-app/src/components/MobileBanner/index.js
+++ react-app/src/components/MobileBanner/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import asanteIcon from '../../assets/asante-icon.png';
 import './MobileBanner.css'
 
 export default function MobileBanner() {
   return (
     <div className='mobile-banner-container'>
+    {/* <div className='mobile-banner-container' style={{backgroundImage: `url(${asanteIcon})`, backgroundSize: "70%", backgroundRepeat: "no-repeat"}}> */}
         <div className='mobile-banner'>Asante was developed to be used as a desktop application</div>
+        <img className='mobile-img' src={asanteIcon} alt="asante-icon" />
     </div>
   )
 }
